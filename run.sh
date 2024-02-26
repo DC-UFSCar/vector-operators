a6303c0fe43962773ac9ebd0350bca7704ad82c9
Rodolfo Azevedo <rodolfo.azevedo@unicamp.br>
2024-02-26 20:05:42 -0300

    atualizando teste

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 iverilog -o tb_somador tb_somador.v somador.v
 ./tb_somador > somador.out
-diff somador.out somador.ok
+# diff somador.out somador.ok
+
+if diff somador.out somador.ok >/dev/null; then
+    echo "OK"
+else
+    echo "ERRO"
+fi
